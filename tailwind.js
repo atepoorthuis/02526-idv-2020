2fff5a8537bcd8f05c8b2c7de127bba95df1f5b2
Ate Poorthuis <atepoorthuis@gmail.com>
2020-01-27 23:50:07 +0800

    feat: basic styling

--- tailwind.js
+++ tailwind.js
@@ -1,5 +1,11 @@
 module.exports = {
   theme: {
+    listStyleType: {
+      none: 'none',
+      disc: 'disc',
+      decimal: 'decimal',
+      circle: 'circle'
+    },
     fontFamily: {
       sans: [
         '"Open Sans"',
